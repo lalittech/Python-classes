2e57a6c02ca196b3c0fcb4451fa2e230632806a4
Black_Hat <39707069+lalittech@users.noreply.github.com>
2019-10-11 22:29:19 +0530

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,28 @@
+
+
+class wiologinDetails():
+    def __init__(self):
+        print("Inside wiologinDetails")
+        
+    
+    def login(self,val):
+        print("args receive is :",val)
+        (Name, UserEmailId, age) = val
+        
+        
+        
+      
+        
+        
+class wioRules():
+    def __init__(self):
+        print("Inside wio-Rules")
+        return
+                
+        
+class wiodeviceDetails():
+    def __init__(self):
+        print("Inside wiodetailsDetails")
+        return
+        
+                
